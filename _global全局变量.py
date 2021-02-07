f4071447db5e9817b2a4d2d5f23d3d8e55820be4
dongyueqian <874974405@qq.com>
2021-02-07 17:32:39 +0800

    又加了一个函数mins()做对比

--- _global全局变量.py
+++ _global全局变量.py
@@ -1,10 +1,24 @@
-num  = 1000
+num1  = 1000
+num2  = 48
 
 def add():
-    global num
-    num = 400
-    return num
+    global num1
+    num1 = 400
+    return num1
 
-print(num)
+def mins():
+    num2 = 99
+    return num2
+
+
+print(num1)
 add()
-print(num) #add()函数中，num加了global ，才会被重新赋值
+print(num1) #add()函数中，num加了global ，才会被重新赋值
+
+print('************************************')
+
+print(num2)
+mins()
+print(num2) # mins()函数中没有给num2添加全局变量关键字，所有没有修改num2的值
+
+
